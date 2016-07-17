b28c69275c0b279d80119aa8483b7685d0595718
panxue <panxue@iyunxiao.com>
2016-07-17 14:18:50 +0800

    2

--- 1. Two Sum/index.js
+++ 1. Two Sum/index.js
@@ -4,15 +4,30 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = [];
+    let args = {};
     for(let i=0; i<nums.length; i++){
-
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j]==target){
-                result.push(i);
-                result.push(j);
+        if(args[nums[i]]==null){
+            args[nums[i]]=[];
+        }
+        args[nums[i]].push(i);
+    }
+    let keys = Object.keys(args);
+    let result = new Set();
+    for(let i=0; i<keys.length; i++){
+        let a = keys[i];
+        let b = target-a+'';
+        if(keys.indexOf(b)>-1){
+            for(let f=0;f<args[a].length; f++){
+                for(let s=0;s<args[b].length; s++){
+                    if(args[a][f]!=args[b][s]){
+                        result.add(args[a][f]);
+                        result.add(args[b][s]);
+                    }
+                }
             }
         }
     }
-    return result;
+    return [...result];
 };
+
+console.log(twoSum([3,2,4],6));
